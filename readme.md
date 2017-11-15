7576f3421658e2c66a4262564635e19c2fe4889f
caojiayuan <cjy632258@hotmail.com>
2017-11-15 23:05:19 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+## AdminLTE + Vue + Laravel
---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="/css/skins/_all-skins.min.css">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div id="app" class="wrapper">
+    <router-view></router-view>
+</div>
+</body>
+<script src="/js/manifest.js"></script>
+<script src="/js/vendor.js"></script>
+<script src="/js/app.js"></script>
+</html>
